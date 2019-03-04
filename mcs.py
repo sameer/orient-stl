3b16b941146a9b036e48638ad8b9e07f3833f6f6
Sameer Puri <sameer@users.noreply.github.com>
2019-03-04 09:48:24 -0600

    Update with comments

--- mcs.py
+++ mcs.py
@@ -1,5 +1,6 @@
 from typing import Dict, Generic, TypeVar, NamedTuple, List
 import math
+import time
 
 from scipy import optimize
 import numpy as np
@@ -7,12 +8,15 @@ from matplotlib import pyplot as plt
 from mpl_toolkits import mplot3d
 from stl import mesh
 
-original_mesh = mesh.Mesh.from_file('stardestroyer-engines.stl')
+original_mesh = mesh.Mesh.from_file('battery mount.stl')
 
 
 def f(x):
+    start = time.time()
+    # need to copy, rotations in-place lose significant precision
     new_mesh = mesh.Mesh(original_mesh.data.copy(), remove_empty_areas=True)
-    # rotate face to match orientation of the build plane
+    
+    # rotate face to match orientation of the build plate
     roll = new_mesh.rotation_matrix([1, 0, 0], x[0])
     pitch = new_mesh.rotation_matrix([0, 1, 0], x[1])
     yaw = new_mesh.rotation_matrix([0, 0, 1], x[2])
@@ -20,25 +24,40 @@ def f(x):
     new_mesh.rotate_using_matrix(rotation_matrix)
     new_mesh.update_normals()
 
-    # find the z-height of the lowest vertex
+    # z-height of the lowest vertex
     lowest_vertex_z = np.amin(new_mesh.vectors[:,:,2])
 
-    # Faces of all the triangles in 2D
+    # faces of all the triangles in 2D, precision bumped to 128-bit where possible
     faces_2d = np.float128(new_mesh.vectors[:,:,:2])
+    # find overhangs that aren't bottom-surfaces
     overhang_face_indices = np.extract((new_mesh.normals[:,2] < 0) * (new_mesh.vectors[:,:,2] > lowest_vertex_z).any(axis=1), np.arange(0, len(faces_2d)))
+    # prune faces to overhangs
     faces_2d = np.take(faces_2d, overhang_face_indices, axis=0)
-    sides = np.array([faces_2d[:, 0] - faces_2d[:, 1], faces_2d[:, 1] - faces_2d[:, 2], faces_2d[:, 2] - faces_2d[:, 0]])
-    lengths = np.linalg.norm(sides, axis=2)
-    lengths.sort(axis=0)
-    c,b,a = lengths[0], lengths[1], lengths[2]
-    heron = (a + (b+c))*(c - (a-b))*(c + (a-b))*(a + (b-c))
+    # sides as vectors
+    sides = np.array([faces_2d[:, i] - faces_2d[:, i+1] for i in range(-1,2)])
+    # vector magnitude
+    side_lengths = np.linalg.norm(sides, axis=2)
+
+    # compute area using numerically stable Heron's formula
+    side_lengths.sort(axis=0)
+    c,b,a = side_lengths[0], side_lengths[1], side_lengths[2]
+    heron = .25 * np.sqrt(np.abs((a + (b+c))*(c - (a-b))*(c + (a-b))*(a + (b-c))))
+    # Rotations produce some triangles that aren't triangles (sum of smaller 2 side lengths < largest side length)
+    # which, when used in Heron's formula, result in sqrt(negative number).
+    # 
+    # Though they should be treated as having zero-area, I treat them as sqrt(abs(number)) * sign(number) 
+    # so that the objective function may be pseudo-convex (right terminology?).
+    heron *= np.sign(heron)
     
-    value = np.sum(.25 * np.sqrt(np.abs(heron)) * np.sign(heron))
-    print(f'Computed f({x})={value}')
+    # sum with pairwise summation, which should keep precision error low
+    value = np.sum(heron)
+
+    finish = time.time()
+    print(f'Computed f({x})={value} in {round((finish-start)*1000)} ms')
     return value
 
 
-res = optimize.minimize(f, method='L-BFGS-B', bounds=[(0, math.pi*2), (0, math.pi*2), (0, math.pi*2)], x0=np.array([4,4,4]), options={'xatol':1E-10})
+res = optimize.minimize(f, method='L-BFGS-B', bounds=[(0, math.pi*2), (0, math.pi*2), (0, math.pi*2)], x0=np.array([0,0,0]), options={'xatol':1E-10})
 print(np.round(res.x, decimals=2))
 
 original_mesh.rotate([1, 0, 0], res.x[0])
